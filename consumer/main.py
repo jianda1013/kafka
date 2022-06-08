03bc8215d47855d6ca4c1e0d9b310597970e2944
jianda <jianda851013@gmail.com>
2022-06-08 23:36:11 +0800

    [change] create channel by sending HTTP request

--- consumer/main.py
+++ consumer/main.py
@@ -1,17 +1,13 @@
-from routes import app
+from routes import app, createChannel
 from livereload import Server
-from flask import render_template
 import requests
 import json
-import _thread
-import wordCloud
 import time
 from os import environ
 
 PRODUCER_PORT = environ.get('PRODUCER_PORT')
 PORT = environ.get('CONSUMER_PORT')
 
-    
 if __name__ == '__main__':
     while True:
         try:
@@ -25,6 +21,6 @@ if __name__ == '__main__':
         time.sleep(5)
     msg = json.loads(channels.text)['msg']
     for item in msg:
-        _thread.start_new_thread(wordCloud.createPic, (item, ))
+        createChannel(item, True)
     server = Server(app.wsgi_app)
     server.serve(host='0.0.0.0', port=PORT)
--- consumer/routes.py
+++ consumer/routes.py
@@ -1,8 +1,33 @@
 import flask
+import _thread
+import requests
+from os import environ
+import wordCloud
 
 app = flask.Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 
+PRODUCER_PORT = environ.get('PRODUCER_PORT')
+allChannel = []
+
+def createChannel(channel, isStart):
+    if channel not in allChannel:
+        try:
+            if not isStart:
+                requests.post('http://producer:'+PRODUCER_PORT+'/channel', json = {'username': 'client', 'channel': channel})
+            _thread.start_new_thread(wordCloud.createPic, (channel, ))
+            allChannel.append(channel)
+        except Exception as e:
+            print(e)
+
 @app.route('/')
 def index():
     return flask.render_template("index.html")
+
+@app.route('/channel', methods=['POST'])
+def newChannel():
+    channel = flask.request.json.get('channel')
+    createChannel(channel, False)
+    return {'msg': 'CHANNEL_CREATED'}
+
+    
